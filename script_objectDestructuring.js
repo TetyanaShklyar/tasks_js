c52300b0321612cc107f4e83e88a82aac9ce3b2b
Tetyana <tetyana.shklyar@gmail.com>
2023-06-19 14:01:02 +0300

    Add script function Object Destructuring

---
+++ script_objectDestructuring.js
@@ -0,0 +1,26 @@
+const personalPlanPeter = {
+  name: 'Peter',
+  age: '29',
+  skills: {
+    languages: ['ru', 'eng'],
+    programmingLangs: {
+      js: '20%',
+      php: '10%',
+    },
+    exp: '1 month',
+  },
+  showAgeAndLangs: function (obj) {
+    let { age } = obj
+    let { languages } = obj.skills
+
+    let str = `Мне ${age} и я владею языками: `
+
+    languages.forEach((lang) => {
+      str += `${lang.toUpperCase()} `
+    })
+
+    return str
+  },
+}
+
+console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter))
